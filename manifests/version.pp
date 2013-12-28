21ed74ba0024dba85b5eb2614baef1e77f83c537
Scott Smith <scott@digital-presence.co.uk>
2013-12-28 10:03:49 +0000

    Added apache24 support for Ubuntu 13.10
    Added module decisions based on apache version. Changed some templates to work with new/deprecated authz modules
    
    Taken default Order directive and changed conf.d Include to IncludeOptional
    
    Add Apache 2.4 with a apache_version variable
    
    Changed the apache_version to be injectable with default 2.2 in params. Fixed IncludeOptional as well as lots of other changes
    
    Added specs for new apache 2.4 implementation
    
    apache_version in alias.pp is injectable with default in apache
    
    Refactored the mpm modules to all use one defined type to make sure everyhing is loaded Apache 2.2 or over
    
    Dont limit needed modules for Apache 2.4 to Debian
    
    Apache version is needed in the mpm module files to be able to inject into apache::mpm
    
    Updated specs to make sure the load files don't get created if apache < 2.4 and made the event mpm test apache version aware
    
    Add version class to try to detect version based on OS and OS version
    
    Change SSL Mutex to work with Apache 2.[24]
    
    Make Apache 2.4 work on Fedora
    
    Fix the ssl erb 'comparison of String with Float failed'
    
    Add to spec tests for worker, prefork and event on Redhat
    
    Fix some specs and alter prefork/worker mpm module
    
    Fix more tests on event and worker
    
    Arrgggh. Take out the test for event in Redhat <2.4
    
    Fix specs

---
+++ manifests/version.pp
@@ -0,0 +1,29 @@
+# Class: apache::version
+#
+# Try to automatically detect the version by OS
+#
+class apache::version {
+$d = 2.2
+  case $::osfamily {
+    'RedHat': {
+      if $::operatingsystem == 'Fedora' and $::operatingsystemrelease >= 18 {
+        $default = 2.4
+      } else {
+        $default = 2.2
+      }
+    }
+    'Debian': {
+      if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease >= 13.10 {
+        $default = 2.4
+      } else {
+        $default = 2.2
+      }
+    }
+    'FreeBSD': {
+      $default = 2.2
+    }
+    default: {
+      fail("Class['apache::version']: Unsupported osfamily: ${::osfamily}")
+    }
+  }
+}
